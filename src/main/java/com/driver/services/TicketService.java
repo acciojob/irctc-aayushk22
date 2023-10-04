cd456c6927a907196c69141a04a6604457f128cc
aayushk22 <ayushkumar22.ak@gmail.com>
2023-10-04 16:56:22 +0530

    changes in book ticket

--- src/main/java/com/driver/services/TicketService.java
+++ src/main/java/com/driver/services/TicketService.java
@@ -4,6 +4,7 @@ package com.driver.services;
 import com.driver.EntryDto.BookTicketEntryDto;
 import com.driver.EntryDto.SeatAvailabilityEntryDto;
 import com.driver.model.Passenger;
+import com.driver.model.Station;
 import com.driver.model.Ticket;
 import com.driver.model.Train;
 import com.driver.repository.PassengerRepository;
@@ -12,10 +13,7 @@ import com.driver.repository.TrainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class TicketService {
@@ -42,9 +40,9 @@ public class TicketService {
         //Incase the train doesn't pass through the requested stations
         //throw new Exception("Invalid stations");
         String [] stations = train.getRoute().split(",");
-        ArrayList<String> stationsAsArrayList = new ArrayList<>(Arrays.asList(stations));
-        if (!stationsAsArrayList.contains(bookTicketEntryDto.getFromStation().toString())
-                || !stationsAsArrayList.contains(bookTicketEntryDto.getToStation().toString())) {
+        ArrayList<String> stationsList = new ArrayList<>(Arrays.asList(stations));
+        if (!stationsList.contains(bookTicketEntryDto.getFromStation().toString())
+                || !stationsList.contains(bookTicketEntryDto.getToStation().toString())) {
             throw new Exception("Invalid stations");
         }
 
@@ -55,7 +53,36 @@ public class TicketService {
         }
         // Incase the there are insufficient tickets
         // throw new Exception("Less tickets are available");
+        int totalSeatsInTrain = train.getNoOfSeats();
+        int availableSeats = 0;
+        Station fromStation = bookTicketEntryDto.getFromStation();
+        Station toStation = bookTicketEntryDto.getToStation();
 
+        HashMap<String,Integer> freqOfPassengersAtStation = new HashMap<>();
+        for (String s: stationsList) {
+            freqOfPassengersAtStation.put(s,0);
+        }
+        List<Ticket> ticketList = train.getBookedTickets();
+
+        for (Ticket t : ticketList) {
+            boolean flag = false;
+            for (String s : stationsList) {
+                if (s.equals(t.getFromStation().toString())) flag = true;
+                if (s.equals(t.getToStation().toString())) break;
+                if (flag) freqOfPassengersAtStation.put(s, freqOfPassengersAtStation.get(s) + t.getPassengersList().size());
+            }
+        }
+
+        boolean bool = false;
+        for (String s: stationsList) {
+            if (s.equals(fromStation.toString())) bool = true;
+            if (s.equals(toStation.toString())) break;
+            if (bool) availableSeats += totalSeatsInTrain - freqOfPassengersAtStation.get(s);
+        }
+
+        if (availableSeats < bookTicketEntryDto.getPassengerIds().size()) {
+            throw new Exception("Less tickets are available");
+        }
 
         //otherwise book the ticket, calculate the price and other details
         List<Integer> passengerIdList = bookTicketEntryDto.getPassengerIds();
@@ -74,8 +101,8 @@ public class TicketService {
         ticket.setTrain(train);
         ticket.setPassengersList(passengerList);
         //Fare System : Check problem statement
-        int intermediateStations = stationsAsArrayList.indexOf(bookTicketEntryDto.getToStation().toString())
-                - stationsAsArrayList.indexOf(bookTicketEntryDto.getFromStation().toString());
+        int intermediateStations = stationsList.indexOf(bookTicketEntryDto.getToStation().toString())
+                - stationsList.indexOf(bookTicketEntryDto.getFromStation().toString());
 
         int totalFare = intermediateStations*300;
 
